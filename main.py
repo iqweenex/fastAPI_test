3b4060456af49ae2d3b5257a4d795d93398823ac
iqweenex <iqweenex@gmail.com>
2025-09-25 10:24:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -152,5 +152,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Только для разработки
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
